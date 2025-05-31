28ba6e1441d7ad44ec788bb3dd1784c8a5160f5b
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-05-31 16:41:52 +0700

    feat(server): add cors middleware implementation

--- server/routes/route.go
+++ server/routes/route.go
@@ -10,22 +10,31 @@ import (
 func SetupRoutes() *gin.Engine {
 	router := gin.Default()
 
+    // CORS middleware
+    router.Use(middleware.SetupCORS())
+
+    // Health check route
+    router.GET("/api/health", func(c *gin.Context) {
+        c.JSON(200, gin.H{"status": "ok"})
+    })
+
+    // Authentication routes
 	router.POST("/api/register", auth.RegisterUser)
     router.POST("/api/login", auth.LoginUser)
 
-    // Get all users
+    // Get all users route
     router.GET("/api/users", middleware.JWTMiddleware(), controllers.UserList)
 
-    // Create a new user
+    // Create a new user route
     router.POST("/api/user", middleware.JWTMiddleware(), controllers.CreateUser)
 
-    // Get an user by ID
+    // Get an user by ID route
     router.GET("/api/user/:id", middleware.JWTMiddleware(), controllers.GetUserByID)
 
-    // Update an user by ID
+    // Update an user by ID rute
     router.PUT("/api/user/:id", middleware.JWTMiddleware(), controllers.UpdateUserByID)
 
-    // Delete an user by ID
+    // Delete an user by ID route
     router.DELETE("/api/user/:id", middleware.JWTMiddleware(), controllers.DeleteUserByID)
 
 	return router
