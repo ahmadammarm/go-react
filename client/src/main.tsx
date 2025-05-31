678654f7e8af5e1fb92ddbb3fd007b61bb467bd1
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-05-31 19:24:26 +0700

    feat(client): add auth context implementation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,16 +5,19 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './context/AuthContext.tsx';
 
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <App />
-            </QueryClientProvider>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                </QueryClientProvider>
+            </BrowserRouter>
+        </AuthProvider>
     </StrictMode>,
 )
