14fdb2a53e37c1fddab52b4ce666c1cc4bd344b1
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-06-02 14:53:21 +0700

    feat(client): add custom hooks for get all users from api

---
+++ client/src/hooks/useUsers.tsx
@@ -0,0 +1,35 @@
+import API from "@/services/api"
+import { useQuery } from "@tanstack/react-query"
+import Cookies from "js-cookie"
+
+export interface User {
+    id: number
+    name: string
+    username: string
+    email: string
+}
+
+export const useUsers = () => {
+    return useQuery<User[], Error>({
+
+        queryKey: ['users'],
+
+        queryFn: async () => {
+            const token = Cookies.get('token')
+
+            const response = await API.get('/api/users', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            if (response.status !== 200) {
+                throw new Error('Failed to fetch users')
+            }
+
+            return response.data as User[]
+        }
+
+
+    })
+}
